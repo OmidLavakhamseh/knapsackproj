6f1965bfa6c4d32c71ac8e42284b11f06bce422b
Omid Lavakhamseh <omidlava2021@gmail.com>
2021-10-12 12:35:03 +0200

    greedy alarm fixed

--- R/greedy_heuristic.r
+++ R/greedy_heuristic.r
@@ -7,40 +7,37 @@
 #' \code{\link{knapsack_dynamic}}
 
 
+
 greedy_knapsack <- function(x, W){
-  stopifnot(inherits(x,"data.frame"))
-  stopifnot(W>0)
-
-  valueRatio = x$v/x$w
-  x$origIndex = 1:nrow(x)
-
-  x=x[order(valueRatio, decreasing=TRUE),]
-
-  combination1=c()
-  combination=c()
-
-  vikt1 = 0
-  vikt = 0
-
-  varde1 = 0
-  varde = 0
-
+  c1=c()
+  c=c()
+  if (inherits(x,"data.frame")==FALSE){
+    stop()
+  }
+  if(W<=0){
+    stop()
+  }
+  v=v1 = 0
+  vrat = x$v/x$w
+  numrow<- nrow(x) 
+  x$OI= 1:numrow
+  vd=vd1 = 0
+  x=x[order(vrat, decreasing=TRUE),]
   i=1
-  while(vikt1<=W){
-    vikt=vikt1
-    varde=varde1
-    combination=combination1
-
-    vikt1=vikt1+x$w[i]
-    varde1=varde1+x$v[i]
-    combination1=c(combination1,i)
+  while((v1<W)|(v1==W)){
+    v=v1
+    vd=vd1
+    c=c1
+    v1=v1+x$w[i]
+    vd1=vd1+x$v[i]
+    c1=c(c1,i)
     i=i+1
   }
 
-  returnList=list(value=round(varde),elements=x$origIndex[combination])
+  greedylist=list(value=round(vd),elements=x$OI[c])
 
 
-  return(returnList)
+  return(greedylist)
 
 }
 
